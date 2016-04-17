17a9195a8099dc05680d768ed8cf956add8eda8b
Laura <lg3295@gmail.com>
2016-04-16 16:57:46 -1000

    creates db named todo_app

--- todo_app.sql
+++ todo_app.sql
@@ -7,3 +7,6 @@ CREATE ROLE michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 -- 3. drop database todo_app
 DROP DATABASE IF EXISTS todo_app;
 
+-- 4. create database todo_app
+CREATE DATABASE todo_app;
+
